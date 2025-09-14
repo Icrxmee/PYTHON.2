e14ac0749dc3919a4a230b29746bf1b8f38038e8
Pedro Ícaro <160777218+Icrxmee@users.noreply.github.com>
2025-09-14 20:15:54 -0300

    Update ANALISADOR COMPLETO.py

--- ANALISADOR COMPLETO.py
+++ ANALISADOR COMPLETO.py
@@ -1,10 +1,10 @@
-somaidade = 0
+somaidade = 0 # Somadores e um acumulador
 mediaidade = 0
 maioridadehome = 0
 nomevelho = ""
 mulher20 = 0
 
-for c in range (1, 5):
+for c in range (1, 5): #Laço de repetição
     
     print (f'---------- {c}° PESSOA ----------')
     
@@ -12,20 +12,22 @@ for c in range (1, 5):
     idade = int ( input("Digite a idade:"))
     sexo = str ( input("Sexo [F/M]: ")).strip()
 
-    somaidade += idade
+    somaidade += idade # Soma para o cálculo da média das idades posterior mente.¹ 
 
-    if c == 1 and sexo in 'Mm':
+    if c == 1 and sexo in 'Mm': # Defiinindo o primeiro homem do laço como o mais velho para ser comparado com o resto.
         maioridadehome = idade
         nomevelho = nome
 
-    if sexo in 'Mm' and idade > maioridadehome:
+    if sexo in 'Mm' and idade > maioridadehome: # Compara cada homem do laço e definine o com a maior idade "inputada" como mais velho
         maioridadehome = idade
         nomevelho = nome 
 
-    if sexo in 'Ff' and idade < 20:
+    if sexo in 'Ff' and idade < 20:  # Soma ao somador (mulher20) sempre que uma mulher tiver menos de 20 anos
         mulher20 += 1
 
-mediaidade = somaidade / 4
+mediaidade = somaidade / 4 # Divisão para a média das idades ²
+
 print (f"A media de idade do grupo é de: {mediaidade}")
 print (f'O homem mais velho tem {maioridadehome} anos e se chama {nomevelho}')
+
 print (f'Temos {mulher20} mulheres menores que 20 anos.')
